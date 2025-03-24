57d99097adece0c780b1337e31d522aac27597dd
Jacob Sills <jsills2592@gmail.com>
2025-03-24 18:11:59 +0000

    added .get for /api/whoami

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+  
+  // Get the user's IP address (from the 'x-forwarded-for' header or the default 'req.ip' if behind a proxy)
+  let ipaddress = req.headers['x-forwarded-for'] || req.ip;
+  // Get the user's preferred language (Accept-Language header)
+  let language = req.headers['accept-language'].split(',')[0]; // Just the first language in the list
+  // Get the user's software (User-Agent header)
+  let software = req.headers['user-agent'];
+
+  // Respond with JSON object including ipaddress, language, and software
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
+
+
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
